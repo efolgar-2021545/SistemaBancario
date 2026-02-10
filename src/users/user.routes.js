af47b1f1aa1395509e272edcfa0dc86a6982b499
alacan-2024010 <alacan-2024010@kinal.edu.gt>
2026-02-10 15:08:41 -0600

    Se agregaron las rutas de los usuarios

---
+++ src/users/user.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { createUser, getUsers } from "./user.controller.js";
+
+const router = new Router();
+
+router.post(
+    '/createUsers',
+    createUser
+)
+
+router.get(
+    '/getUsers',
+    getUsers
+)
