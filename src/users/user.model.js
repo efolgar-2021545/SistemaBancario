b33f3e5902158f9a31be388080f98bb1dfd7edea
aalvarez-2024004 <aalvarez-2024004@kinal.edu.gt>
2026-02-11 13:23:41 -0600

    Se corrigio la funcion que encripta las contrasenas debido a que no exisitia ninguna funcion llamada next

--- src/users/user.model.js
+++ src/users/user.model.js
@@ -82,16 +82,11 @@ const userSchema = new mongoose.Schema(
 //Antes de guardar un usuario se ejecuta esta funcion
 //Si la contra no fue modificada, entonces no se encripta otra vez
 // Esta funcion reemplaza la contrasena original y la encripta por medio de bycrypt
-userSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
-    
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (error) {
-        next(error);
-    }
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 export default mongoose.model('User', userSchema);
