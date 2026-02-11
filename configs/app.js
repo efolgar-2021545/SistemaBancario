8738c6aff632a060c9f3deb7fcd43ef48426c28e
aalvarez-2024004 <aalvarez-2024004@kinal.edu.gt>
2026-02-11 14:09:18 -0600

    Se realizo la creacion automatica del administrador, se realizo el controlador de autenticacion para comporobar si el usuario existe o no, es decir el login

--- configs/app.js
+++ configs/app.js
@@ -9,6 +9,8 @@ const BASE_PATH = '/kinalBank/v1';
 
 //Se importaran todos los routes de las entidades
 import userRoutes from '../src/users/user.routes.js';
+import authRoutes from '../src/auth/auth.routes.js';
+
 
 export const initApp = () => {
   const app = express();
@@ -19,6 +21,7 @@ export const initApp = () => {
   app.use(morgan('dev'));
 
   app.use(BASE_PATH, userRoutes);
+  app.use(BASE_PATH, authRoutes);
 
   // Endpoint de prueba
   app.get(`${BASE_PATH}/health`, (req, res) => {
--- index.js
+++ index.js
@@ -1,15 +1,52 @@
 import dotenv from 'dotenv';
 import { initApp } from './configs/app.js';
 import { dbConnection } from './configs/database.js';
+import User from './src/users/user.model.js';
+import { generateAccountNumber } from './src/helpers/account-number.js';
 
 dotenv.config();
 
 const app = initApp();
-
 const PORT = process.env.PORT || 3000;
 
-dbConnection();
+// Crear ADMIN automaticamente
+const createAdmin = async () => {
+  try {
+    const adminExists = await User.findOne({ username: 'ADMINB' });
 
-app.listen(PORT, () => {
-  console.log(`Kinal Bank API corriendo en puerto ${PORT}`);
-});
+    if (!adminExists) {
+      await User.create({
+        name: 'Administrador',
+        username: 'ADMINB',
+        accountNumber: generateAccountNumber(),
+        dpi: '0000000000000',
+        direction: 'Kinal',
+        phone: '00000000',
+        email: 'admin@kinalbank.com',
+        password: 'ADMINB',
+        jobName: 'Administrador General',
+        monthlyIncome: 1000,
+        role: 'ADMIN'
+      });
+
+      console.log('ADMIN creado automáticamente');
+    } else {
+      console.log('ADMIN ya existe');
+    }
+
+  } catch (error) {
+    console.error('Error creando ADMIN:', error.message);
+  }
+};
+
+// Iniciar el  servidor
+const startServer = async () => {
+  await dbConnection();
+  await createAdmin();
+
+  app.listen(PORT, () => {
+    console.log(`Kinal Bank API corriendo en puerto ${PORT}`);
+  });
+};
+
+startServer();
