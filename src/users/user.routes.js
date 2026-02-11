fa555dd8a993e4e5cfff88e6dc55174f7bce9c10
alacan-2024010 <alacan-2024010@kinal.edu.gt>
2026-02-11 17:40:17 -0600

    Se modificaron las rutas de usuario para agregar el validateJWT

--- src/users/user.routes.js
+++ src/users/user.routes.js
@@ -1,15 +1,18 @@
 import { Router } from "express";
 import { createUser, getUsers } from "./user.controller.js";
+import { validateJWT } from "../middlewares/validate-jwt.js";
 
 const router = new Router();
 
 router.post(
     '/createUsers',
+    validateJWT,
     createUser
 )
 
 router.get(
     '/getUsers',
+    validateJWT,
     getUsers
 )
 
